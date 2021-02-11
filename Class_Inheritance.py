1b0f1c5be19022c8f80c6295957c7c109e25f163
Alexander <55025428+ZubritskiyAlex@users.noreply.github.com>
2021-02-11 15:00:49 +0300

    Class_Inheritance

--- Class_Inheritance.py
+++ Class_Inheritance.py
@@ -52,3 +52,73 @@ for _ in range(q):
     parent = question[0]
     child = question[1]
     print(answer(classes, parent, child))
+    
+    
+    
+    
+    
+    """Реализуйте структуру данных, представляющую собой расширенную структуру стек. Необходимо поддерживать добавление элемента на вершину стека, удаление с вершины стека, и необходимо поддерживать операции сложения, вычитания, умножения и целочисленного деления.
+
+Операция сложения на стеке определяется следующим образом. Со стека снимается верхний элемент (top1), затем снимается следующий верхний элемент (top2), и затем как результат операции сложения на вершину стека кладется элемент, равный top1 + top2.
+
+Аналогичным образом определяются операции вычитания (top1 - top2), умножения (top1 * top2) и целочисленного деления (top1 // top2).
+
+Реализуйте эту структуру данных как класс ExtendedStack, отнаследовав его от стандартного класса list."""
+
+
+class ExtendedStack(list):
+    def sum(self):
+        op1, op2 = self.pop(), self.pop()
+        self.append(op1 + op2)
+
+    def sub(self):
+        op1, op2 = self.pop(), self.pop()
+        self.append(op1 - op2)
+
+    def mul(self):
+        op1, op2 = self.pop(), self.pop()
+        self.append(op1 * op2)
+
+    def div(self):
+        op1, op2 = self.pop(), self.pop()
+        self.append(op1 // op2)
+
+def test():
+    ex_stack = ExtendedStack([1, 2, 3, 4, -3, 3, 5, 10])
+    ex_stack.div()
+    assert ex_stack.pop() == 2
+    ex_stack.sub()
+    assert ex_stack.pop() == 6
+    ex_stack.sum()
+    assert ex_stack.pop() == 7
+    ex_stack.mul()
+    assert ex_stack.pop() == 2
+    assert len(ex_stack) == 0
+
+if __name__ == "__main__":
+    test()
+    
+    
+    
+    
+    
+    
+ """Реализуйте класс LoggableList, отнаследовав его от классов list и Loggable таким образом, чтобы при добавлении элемента в список посредством метода append в лог отправлялось сообщение, состоящее из только что добавленного элемента.
+"""
+
+
+import time
+
+
+class LoggableList(list, Loggable):
+    def append(self, x):
+        self.log(x)
+        super().append(x)
+
+def test():
+    t = LoggableList()
+    t.append(1)
+    t.append('str')
+
+    assert len(t) == 2
+
